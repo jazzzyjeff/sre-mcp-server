1fe5693a9c8c2e37228425bd142167cf89aa4e3a
jazzytoor <32848916+jazzzyjeff@users.noreply.github.com>
2026-04-18 15:43:25 +0100

    feat: add in more k8s tools

--- k8s/models.py
+++ k8s/models.py
@@ -9,3 +9,21 @@ class Pod(BaseModel):
     node: Optional[str] = None
     restart_count: int
 
+
+class Deployment(BaseModel):
+    name: str
+    namespace: str
+    desired: int
+    ready: int
+    available: int
+    image: str
+
+
+class Event(BaseModel):
+    namespace: str
+    name: str
+    reason: str
+    message: str
+    count: int
+    first_time: Optional[str] = None
+    last_time: Optional[str] = None
